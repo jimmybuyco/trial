366284c612b2ec065d5d42abba90f37310302cf5
jimmybuyco <jimmybuyco@gmail.com>
2017-03-28 08:38:54 +0800

    update migration

--- database/migrations/2017_03_27_050322_create_billers_table.php
+++ database/migrations/2017_03_27_050322_create_billers_table.php
@@ -17,6 +17,7 @@ class CreateBillersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('description');
+            $table->integer('category');
             $table->timestamps();
         });
     }
