98a8108e580ed9a9bd627dcc3621ee59f7b6ce4e
jimmybuyco <jimmybuyco@gmail.com>
2017-03-28 10:52:32 +0800

    update

--- resources/views/layouts/default/footer.blade.php
+++ resources/views/layouts/default/footer.blade.php
@@ -9,28 +9,3 @@
 </body>
 </html>
 
-<script type="text/javascript">
-    var example1 = new Vue({
-        el: '#example-1',
-        data: {
-            ctr: 0,
-            billers: []
-        },
-        methods: {
-            submit: function (e) {
-                alert('Hello ' + this.name + '!');
-            },
-            getBiller: function (e) {
-                $.ajax({
-                    url: 'getAllBiller',
-                    method: 'get',
-                    success: function(e) {
-                        example1.billers = e;
-                    }
-                });
-            }
-        }
-    });
-
-    example1.getBiller();
-</script>
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,10 @@
 
 Route::match(['get', 'post'], '/',               'HomeController@home');
 Route::match(['get', 'post'], '/add-bill',       'HomeController@addBill');
+Route::match(['get', 'post'], '/add-category',   'CategoryController@addCatergory');
+
 Route::match(['get', 'post'], '/get-all-biller', 'BillerController@getAllBiller');
+Route::match(['get', 'post'], '/get-all-category', 'CategoryController@getAllCategory');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
