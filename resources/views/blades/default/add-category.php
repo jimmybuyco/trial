c5463f95858d6515e98f4b0e3197eee7914cdc9c
jimmybuyco <jimmybuyco@gmail.com>
2017-03-28 12:26:47 +0800

    update

---
+++ resources/views/blades/default/add-category.php
@@ -0,0 +1,68 @@
+<?php include resource_path('views/layouts/default/header.blade.php'); ?>
+
+<div class="container">
+    <div id="category">
+        <table>
+            <tr>
+                <td>Name</td>
+                <td><input name="name" type="text" v-model="categoryname"></td>
+            </tr>
+            <tr>
+                <td>Description</td>
+                <td><input name="description" type="text" v-model="categorydescription"></td>
+            </tr>
+        </table>
+        <button type="button" v-on:click="submit">Submit</button>
+        <hr>
+        <table border="1">
+            <tr>
+                <td>Id</td>
+                <td>Name</td>
+                <td>Description</td>
+            </tr>
+            <tr v-for="category of categories">
+                <td>{{category.id}}</td>
+                <td>{{category.name}}</td>
+                <td>{{category.description}}</td>
+            </tr>
+        </table>
+    </div>
+</div>
+
+<?php include resource_path('views/layouts/default/footer.blade.php'); ?>
+
+<script type="text/javascript">
+    var categoryVue = new Vue({
+        el: '#category',
+        data: {
+            ctr: 0,
+            categories: [],
+            categoryname: '',
+            categorydescription: ''
+        },
+        methods: {
+            submit: function (e) {
+                $.ajax({
+                    url: 'add-category?name=' + this.categoryname + "&desc=" + this.categorydescription,
+                    type: 'post',
+                    success: function (e) {
+                        categoryVue.categories.unshift(e)
+                        categoryVue.categoryname='';
+                        categoryVue.categorydescription='';
+                    }
+                });
+            },
+            getCategories: function (e) {
+                $.ajax({
+                    url: 'get-all-category',
+                    method: 'get',
+                    success: function (e) {
+                        categoryVue.categories = e;
+                    }
+                });
+            }
+        }
+    });
+
+    categoryVue.getCategories();
+</script>
