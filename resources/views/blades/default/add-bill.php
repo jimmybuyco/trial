cca2950e3a2fa77dcc7a4279c37f32b9b432fb3e
jimmybuyco <jimmybuyco@gmail.com>
2017-03-31 15:04:51 +0800

    up

--- resources/views/blades/default/add-bill.php
+++ resources/views/blades/default/add-bill.php
@@ -42,15 +42,15 @@
 
             <div class="col-md-8 row" style="overflow: scroll;height:500px;border: 1px solid black;">
                 <div class="col-md-2" style="border: 1px solid black;margin: 10px" v-for="bill of allBills">
-                    <h5><?php // echo array_column($bill, 'id')?></h5>
-                    <h6>{{ bill.categoriesName }}</h6>
+
+                    <h5>{{billerName(bill.biller_id)}}</h5>
+                    <h5>{{categoryName(bill.biller_id)}}</h5>
                     <h6>{{ bill.account_number }}<h6>
-                    <h6>{{ bill.amount }}<h6>
+                            <h6>{{ bill.amount }}<h6>
                 </div>
             </div>
         </div>
-    </div>
 
-    <script src="js/default/add-bill.js"></script>
+        <script src="js/default/add-bill.js"></script>
 
 <?php include resource_path('views/layouts/default/footer.blade.php'); ?>
